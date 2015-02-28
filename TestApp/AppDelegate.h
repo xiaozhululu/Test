9106ce0cec30a064e7dba0bac5e873e03dbbf68b
xiaozhululu <nsf724947554@vip.qq.com>
2015-02-28 10:26:03 +0800

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by NSF on 15/2/28.
+//  Copyright (c) 2015年 NSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
